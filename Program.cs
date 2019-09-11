98afd137eb714409a98dc10bfb0ff4a0fb0ad94a
jacobsendk <55154589+jacobsendk@users.noreply.github.com>
2019-09-11 16:49:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DB.MaxSum
+{
+    /// <summary>
+    /// Command line utility that calculates pyramid data.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pyramid = new PyramidGraph(GetPyramidData());
+            Console.WriteLine("Max sum is " + pyramid.GetMaxSumOfGraph());
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Returns pyramid test data.
+        /// </summary>
+        /// <returns>List of rows with data starting with top of the pyramid as first row.</returns>
+        private static List<int[]>  GetPyramidData()
+        {
+            var pyramidRows = new List<int[]>();
+            pyramidRows.Add(new int[] { 215 });
+            pyramidRows.Add(new int[] { 192, 124 });
+            pyramidRows.Add(new int[] { 117, 269, 442 });
+            pyramidRows.Add(new int[] { 218, 836, 347, 235 });
+            pyramidRows.Add(new int[] { 320, 805, 522, 417, 345 });
+            pyramidRows.Add(new int[] { 229, 601, 728, 835, 133, 124 });
+            pyramidRows.Add(new int[] { 248, 202, 277, 433, 207, 263, 257 });
+            pyramidRows.Add(new int[] { 359, 464, 504, 528, 516, 716, 871, 182 });
+            pyramidRows.Add(new int[] { 461, 441, 426, 656, 863, 560, 380, 171, 923 });
+            pyramidRows.Add(new int[] { 381, 348, 573, 533, 448, 632, 387, 176, 975, 449 });
+            pyramidRows.Add(new int[] { 223, 711, 445, 645, 245, 543, 931, 532, 937, 541, 444 });
+            pyramidRows.Add(new int[] { 330, 131, 333, 928, 376, 733, 017, 778, 839, 168, 197, 197 });
+            pyramidRows.Add(new int[] { 131, 171, 522, 137, 217, 224, 291, 413, 528, 520, 227, 229, 928 });
+            pyramidRows.Add(new int[] { 223, 626, 034, 683, 839, 052, 627, 310, 713, 999, 629, 817, 410, 121 });
+            pyramidRows.Add(new int[] { 924, 622, 911, 233, 325, 139, 721, 218, 253, 223, 107, 233, 230, 124, 233 });
+            return pyramidRows;
+        }
+    }
+}
---
+++ PyramidGraph.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DB.MaxSum
+{
+    /// <summary>
+    /// Holds a pyramid of graph data and allows calculation of different statistics.
+    /// </summary>
+    class PyramidGraph
+    {
+        private List<int[]> pyramidRows;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="pyramidRows"></param>
+        public PyramidGraph(List<int[]> pyramidRows)
+        {
+            this.pyramidRows = pyramidRows;
+        }
+
+        /// <summary>
+        /// Gets the maximum sum of graph paths in a pyramid dataset.
+        /// </summary>
+        /// <returns>Max sum</returns>
+        public int GetMaxSumOfGraph()
+        {
+            return GetMaxSumOfGraph(0, 0);
+        }
+        
+        private int GetMaxSumOfGraph(int rowIndex, int columnIndex)
+        {
+            var row = pyramidRows[rowIndex];
+            int value = row[columnIndex];
+
+            if (rowIndex == pyramidRows.Count - 1)
+            {
+                return value;
+            }
+            else
+            {
+                bool valueIsEven = IsEven(value);
+                var nextRow = pyramidRows[rowIndex + 1];
+                var sumOfPath1 = IsEven(nextRow[columnIndex]) != valueIsEven ? GetMaxSumOfGraph(rowIndex + 1, columnIndex) : 0;
+                var sumOfPath2 = IsEven(nextRow[columnIndex + 1]) != valueIsEven ? GetMaxSumOfGraph(rowIndex + 1, columnIndex + 1) : 0;
+                return value + Math.Max(sumOfPath1, sumOfPath2);
+            }
+        }
+
+        private bool IsEven(int value)
+        {
+            return (value % 2 == 0);
+        }
+    }
+}
